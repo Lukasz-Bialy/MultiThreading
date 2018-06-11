1da79f9722cc90a093ddafc938f1cab4e4f0bd1b
Lukasz Bialy <bialylukasz61@gmail.com>
2018-06-11 13:08:00 +0200

    MultiThreading version 1.0

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,11 +1,13 @@
 public class A {
     public static int iczba = 0;
+
     public static void run() {
         count();
     }
-    private static void count(){
-        for(int i = 0;i<100;i++){
-            iczba+=1;
+
+    private static void count() {
+        for (int i = 0; i < 100; i++) {
+            iczba += 1;
             System.out.println(iczba);
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,10 @@ public class Main {
         final Klient kl1 = new Klient();
 
         Thread t1 = new Thread(kl1);
-
-
         t1.start();
         Thread.sleep(1000);
         kl1.flag = true;
         Thread.sleep(1000);
         kl1.flag = false;
-        // System.out.println(kl1.liczba);
-       /* A klasa = new A();
-        long i = System.currentTimeMillis();
-        A.run();
-        A.run();
-        System.out.println((System.currentTimeMillis() - i));*/
     }
 }

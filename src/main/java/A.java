aca2b607d65ed8b6dc52b57397dd7cc1c4879e98
Lukasz Bialy <bialylukasz61@gmail.com>
2018-06-11 13:03:49 +0200

    MultiThreading version 1.0

---
+++ src/main/java/A.java
@@ -0,0 +1,12 @@
+public class A {
+    public static int iczba = 0;
+    public static void run() {
+        count();
+    }
+    private static void count(){
+        for(int i = 0;i<100;i++){
+            iczba+=1;
+            System.out.println(iczba);
+        }
+    }
+}
